8205d05b8d64b781b5f1b29efb7976a35e4b28a2
Marcelo Mendez <marceloexc@Marcelos-MacBook-Pro.local>
2020-02-24 09:19:52 -0600

    fixed timing

--- marceloexc song bot.py
+++ marceloexc song bot.py
@@ -2,19 +2,29 @@ import spotipy
 import spotipy.util as util
 import tweepy
 import pyperclip
-
 import os
 import json
+import time
+import random
+scopes = 'user-library-read playlist-modify-private playlist-modify-public user-library-modify streaming user-read-playback-state'
+spotify_client_id = '44743ab488e74b718e7e314601de135e'
+spotify_secret = '6375842d113d4679a84196ae54b85034'
+spotify_redirect_url = 'https://www.google.com/'
 username1 = "zanealgogh"
-scope1 = 'user-library-read playlist-modify-private playlist-modify-public user-library-modify streaming user-read-playback-state'
-token = util.prompt_for_user_token(username=username1,scope=scope1,client_id='44743ab488e74b718e7e314601de135e',client_secret='6375842d113d4679a84196ae54b85034',redirect_uri='https://www.google.com/')
+token = util.prompt_for_user_token(username=username1,scope=scopes,client_id=spotify_client_id,client_secret=spotify_secret,redirect_uri=spotify_redirect_url)
 spotifyObject = spotipy.Spotify(auth=token)
 user = spotifyObject.current_user()
 print('Spotify User ' + username1 + ' successfully authenticated')
+#authenticate twitter user
+twitter_consumer = 'OuMcb2WRlqqJwcaGExVWzGGTL'
+twitter_consumer_secret = 'nU2VyQqKg3TZokl3uhJprerhiKBnzux3azPUU2eGaI3N4ArzdB'
+twitter_access = '1230147815877337088-XuFuvqM7RSbA0gV20EAdxIIMGqKnqS'
+twitter_access_secret = '9RgSlaausGllPlq6wBG2b7JkGzzy0Tyd8j5n8Cw2ejUt7'
+auth = tweepy.OAuthHandler(twitter_consumer, twitter_consumer_secret)
+auth.set_access_token(twitter_access, twitter_access_secret)
+api = tweepy.API(auth)
 def CurrentSongPlaying():
     songplaying = spotifyObject.currently_playing()
-    #print(json.dumps(songplaying, sort_keys=True, indent=4))
-    pyperclip.copy(json.dumps(songplaying, sort_keys=True, indent=8))
     currentArtist = songplaying['item']['album']['artists'][0]['name']
     currentAlbum = songplaying['item']['album']['name']
     currentTitle = songplaying['item']['name']
@@ -22,5 +32,20 @@ def CurrentSongPlaying():
     actuallyPlaying = songplaying['is_playing']
     print('Marcelo is currently playing "' + currentTitle + '" by artist ' + currentArtist + ' ' + currentURL)
     print(actuallyPlaying)
+    CurrentTitleMem = currentTitle
+    api.update_status('Marcelo is currently playing "' + currentTitle + '" by artist ' + currentArtist + ' ' + currentURL + ' #' + str(random.randint(1, 10000)))
 
-CurrentSongPlaying()
+
+MemSong = ['']
+while True:
+    songplaying2 = spotifyObject.currently_playing()
+    currentTitle2 = songplaying2['item']['name']
+    if MemSong[-1] == currentTitle2:
+        print('The Same')
+    if MemSong[-1] != currentTitle2:
+        MemSong.append(currentTitle2)
+        print('not')
+        CurrentSongPlaying()
+    time.sleep(10)
+    
+
